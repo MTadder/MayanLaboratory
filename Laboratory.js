0abe2800913fabd9e693426594302dfc38293c8f
MTadder <MTadder@protonmail.com>
2020-04-25 00:13:16 +0000

    testing the HTML5 Canvas API

--- Laboratory.js
+++ Laboratory.js
@@ -1,5 +1,43 @@
-document.onkeypress = function (e) {
-    e = e || window.event;
-    // use e.keyCode
-    alert(e.keyCode);
+const __MayanLab = {
+    Version: "projectar",
+    Author: "MTadder",
+};
+class LabCanvas {
+
+};
+class Laboratory {
+    displayCanvas = undefined;
+    displayContext = undefined;
+
+    debug = {
+        enabled: true,
+        render(mayanContext) {
+            if (mayanContext != undefined && this.enabled) {
+                let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+                mayanContext.fillStyle = "white";
+                mayanContext.font = "22pt monospace";
+                mayanContext.fillText(`Mayan Lab v${__MayanLab.Version} - Chrome? (${isChrome})`, 10,10);
+            };
+        },
+    };
+
+    resolveContext() {
+        if (this.displayCanvas != undefined) {
+            this.displayContext = this.displayCanvas.getContext("2d");
+            return true;
+        } else {
+            console.log("Cannot resolve Context: Canvas is undefined!");
+        };
+        return false;
+    };
+
+    constructor() {};
+};
+
+function Initialize(){
+    let Lab = new Laboratory();
+
+    Lab.displayCanvas = document.getElementById("MayanCanvas");
+    Lab.resolveContext();
+    Lab.debug.render();
 };
